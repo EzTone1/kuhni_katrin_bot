325c9c36174fb2468d4c06e5beae48edbdff55a8
EzTone1 <34780606+EzTone1@users.noreply.github.com>
2023-07-03 16:28:17 +0300

    Добавил картинки и медиагруппу

--- files.py
+++ files.py
@@ -1,4 +1,4 @@
-from aiogram.types import FSInputFile
+from aiogram.types import FSInputFile, InputMediaPhoto
 
 catalog_of_kuhni_katrin = FSInputFile("data/katalogvygodnyhpredlozhenijkatrin.pdf")
 clasic_kitchen = FSInputFile("data/pictures/classic_kitchen.jpg")
@@ -13,3 +13,20 @@ scandinavian_kitchen = FSInputFile("data/pictures/scandinavian.jpg")
 style_chosen = FSInputFile("data/steps/01_step.jpg")
 form_chosen = FSInputFile("data/steps/02_step.jpg")
 tabletop_chosen = FSInputFile("data/steps/03_step.jpg")
+
+
+
+step4_1 = InputMediaPhoto(type="photo", media=FSInputFile("data/firing/step4_1.jpg"))
+step4_2 = InputMediaPhoto(type="photo", media=FSInputFile("data/firing/step4_2.jpg"))
+step4_3 = InputMediaPhoto(type="photo", media=FSInputFile("data/firing/step4_3.jpg"))
+step4_4 = InputMediaPhoto(type="photo", media=FSInputFile("data/firing/step4_4.jpg"))
+step4_5 = InputMediaPhoto(type="photo", media=FSInputFile("data/firing/step4_5.jpg"))
+step4_6 = InputMediaPhoto(type="photo", media=FSInputFile("data/firing/step4_6.jpg"))
+step4_7 = InputMediaPhoto(type="photo", media=FSInputFile("data/firing/step4_7.jpg"))
+step4_8 = InputMediaPhoto(type="photo", media=FSInputFile("data/firing/step4_8.jpg"))
+media_group = [step4_1, step4_2, step4_3, step4_5]
+
+
+
+step3 = FSInputFile("data/firing/step3.jpg")
+step1 = FSInputFile("data/firing/step1.jpg")
--- sheduled_message.py
+++ sheduled_message.py
@@ -2,20 +2,32 @@ from aiogram.types import Message, CallbackQuery
 from aiogram import Bot
 from aiogram.fsm.context import FSMContext
 
+import files
 import kb
 import text
 
 
 async def send_1_fire_message(chat_id: int, bot: Bot):
+    await bot.send_photo(chat_id=chat_id, photo=files.step1)
     await bot.send_message(chat_id, text.fire_message_1, parse_mode="Markdown", reply_markup=kb.firing_reply.as_markup(resize_keyboard=True, one_time_keyboard=True))
 
 async def send_2_fire_message(chat_id: int, bot: Bot):
     await bot.send_message(chat_id, text.fire_message_2, parse_mode="Markdown", reply_markup=kb.firing_reply.as_markup(resize_keyboard=True, one_time_keyboard=True))
 
 async def send_3_fire_message(chat_id: int, bot: Bot):
+    await bot.send_photo(chat_id=chat_id, photo=files.step3)
     await bot.send_message(chat_id, text.fire_message_3, parse_mode="Markdown", reply_markup=kb.firing_reply.as_markup(resize_keyboard=True, one_time_keyboard=True))
 
 async def send_4_fire_message(chat_id: int, bot: Bot):
+    await bot.send_media_group(chat_id=chat_id, media=files.media_group)
+    # await bot.send_photo(chat_id=chat_id, photo=files.step4_1)
+    # await bot.send_photo(chat_id=chat_id, photo=files.step4_2)
+    # await bot.send_photo(chat_id=chat_id, photo=files.step4_3)
+    # await bot.send_photo(chat_id=chat_id, photo=files.step4_4)
+    # await bot.send_photo(chat_id=chat_id, photo=files.step4_5)
+    # await bot.send_photo(chat_id=chat_id, photo=files.step4_6)
+    # await bot.send_photo(chat_id=chat_id, photo=files.step4_7)
+    # await bot.send_photo(chat_id=chat_id, photo=files.step4_8)
     await bot.send_message(chat_id, text.fire_message_4, parse_mode="Markdown", reply_markup=kb.firing2_reply.as_markup(resize_keyboard=True, one_time_keyboard=True))
 
 async def send_5_fire_message(chat_id: int, bot: Bot):
